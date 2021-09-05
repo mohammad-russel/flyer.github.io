487cbf2e116b6513b7f5f240cef31954e6bda4fb
mohammad-russel <83851125+mohammad-russel@users.noreply.github.com>
2021-09-05 11:02:40 +0600

    Add files via upload

---
+++ client.js
@@ -0,0 +1,13 @@
+document.querySelector(".contect").onclick = cu;
+
+function cu() {
+    document.querySelector(".maincfblock").style.display = "block";
+
+}
+
+
+document.querySelector(".cross").onclick = cls;
+
+function cls() {
+    document.querySelector(".maincfblock").style.display = "none";
+}
